b97d8c9f416603f8f61e2583ff8c2cabf4d67dd9
Arnav Raj <arnavraj22.official@gmail.com>
2021-05-23 22:44:35 +0530

    productlisting & product displaying

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import { Products } from "./components/Products";
 import { Cart } from "./components/Cart";
 import { Wishlist } from "./components/Wishlist";
 
-
 function App() {
   const [route, setRoute] = useState("home");
 
   return (
     <div className="App">
-
-      <div className="navbar">
-        <div className="navbar-left-component">
-          <button onClick={() => setRoute("home")}>Logo</button>
-          <button onClick={() => setRoute("products")}>Products</button>
-        </div>
-        <div className="navbar-right-component">
-          <button onClick={() => setRoute("cart")}>Cart</button>
-          <button onClick={() => setRoute("wishlist")}>Wishlist</button>
-        </div>
+      <div className="navbar-left-component">
+        <button onClick={() => setRoute("home")}>Logo</button>
+        <button onClick={() => setRoute("products")}>Products</button>
       </div>
-
+      <div className="navbar-right-component">
+        <button onClick={() => setRoute("cart")}>Cart</button>
+        <button onClick={() => setRoute("wishlist")}>Wishlist</button>
+      </div>
       {route === "home" && <Home />}
       {route === "products" && <Products />}
       {route === "cart" && <Cart />}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { v4 as uuidv4 } from 'uuid';
-
-const productList = [
-    {
-        id :uuidv4(),
-        name: "cloth"
-    }
-]
+import { productList } from "./ProductList";
+import { BiRupee } from "react-icons/bi";
 
 export function Products() {
     return (
-        <div>
+        <>
             <h1>This is Products</h1>
-        </div>
+            <div className="product-side-bar">
+                this will contain sorting and filter
+            </div>
+            <div className="product-listing-container">
+                {productList.map(({ id, imageUrl, name, price, mrp, rating }) => (
+                    <div className="product-display-card" key={id}>
+                        <div className="image-wishBtn-container">
+                            <img src={imageUrl} alt="" />
+                        </div>
+                        <h3 className="product-name">{name}</h3>
+                        <p className="product-price">< BiRupee /> {price} <span style={{ textDecoration: "line-through" }}> {mrp}</span></p>
+                        <p className="product-rating"> {rating}</p>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
